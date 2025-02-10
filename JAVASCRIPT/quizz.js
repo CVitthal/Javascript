d77123812daa0f74de0ed22c6d64ce8cd44b6ae3
CVitthal <vitthal.3392@gmail.com>
2025-02-10 17:40:44 +0530

    Update quizz.js
    Added one more question

--- JAVASCRIPT/quizz.js
+++ JAVASCRIPT/quizz.js
@@ -21,13 +21,20 @@ answer:[
 {answer:"first",correct:false},
 {answer:"first",correct:false}
 ]},
+{question:"this is question4",
+answer:[
+{answer:"first",correct:false},
+{answer:"first",correct:true},
+{answer:"first",correct:false},
+{answer:"first",correct:false}
+]}
 ];
 function start(currentQ){
 	const quest = document.getElementById("question");
 	quest.innerHTML=questions[currentQ].question;
 	let ans="";
 	const answer=document.getElementById("answer");
-	for(let i=0;i<4;i++){
+	for(let i=0;i<5;i++){
 	ans=ans+("<input type='radio' name='op' value="+questions[currentQ].answer[i].correct+">"+questions[currentQ].answer[i].answer+"</br>");
 		}
 	answer.innerHTML=ans;
